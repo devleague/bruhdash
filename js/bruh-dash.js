46b5dee3c9354c58f2da9b21389143ea66c6c5d8
Lisa <misslisazhou@gmail.com>
2016-05-09 20:28:18 -1000

    create .difference() method

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -19,8 +19,21 @@ global.bruhdash = {
     return newArr;
   },
 
-  difference: function() {
-
+  difference: function(arr, compareArr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      var found = false;
+      for (var j = 0; j < compareArr.length; j++) {
+        if (arr[i] === compareArr[j]){
+          found = true;
+          break;
+        }
+      }
+      if (found === false){
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
   },
 
   drop: function(){
