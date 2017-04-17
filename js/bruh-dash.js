d2355db2bae13b1a3f0b1050dd3b4d6c7b83ad06
lorecrafting <raymond.n.luong@gmail.com>
2017-04-16 19:50:01 -1000

    add short function descriptions

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -1,102 +1,129 @@
 var global = window || GLOBAL;
 
 global.bruhdash = {
-  chunk: function(){
-    
-  },
 
-  compact: function() {
-
-  },
-
-  difference: function() {
-
-  },
-
-  drop: function(){
-
-  },
-
-  dropRight: function() {
-
-  },
-
-  fill: function() {
-
-  },
-
+  // returns the first element of an array
   first: function () {
       
   },
 
+  // returns the last element of an array
+  last: function () {
+
+  },
+  
+  // returns an array with all falsey values removed
+  compact: function() {
+
+  },
+
+  // returns an array with specified values excluded
+  difference: function() {
+
+  },
+
+  // returns a slice of array with n elements dropped from the beignning
+  drop: function(){
+
+  },
+
+  // returns a slice of array with n elements dropped from the end
+  dropRight: function() {
+
+  },
+
+  // fills elements of array with specified value from the start index
+  // up to but not including the end index
+  fill: function() {
+
+  },
+
+  // returns the index of the first matching element from left to right
   indexOf: function () {
 
   },
 
-  inital: function () {
+  // returns an array with all elements except for the last element
+  initial: function () {
 
   },
 
-  last: function () {
-
-  },
-
+  
+  // returns the index of the first matching element from left to right
   lastIndexof: function () {
 
   },
 
+  // removes all given values from an array
   pull: function () {
 
   },
 
+  // removes elements of an array corresponding to the given indices
   pullAt: function () {
 
   },
 
-  rest: function () {
-
-  },
-
+  // creates a slice of an array from the start index up to but not including the end index
   slice: function () {
 
   },
 
+  // creates a slice of an array with n elements taken from the beginning
   take: function () {
 
   },
 
+  // creates a slice of an array with n elements taken from the end
   takeRight: function () {
 
   },
 
+  // creates an array of grouped elements
   zip: function () {
 
   },
 
+  // creates an array of grouped elements in their pre-zip configuration
   unzip: function () {
 
   },
 
+  // creates an array excluding all the specified values
   without: function() {
 
   },
 
+  // creates an array of elements into groups of length of specified size
+  chunk: function(){
+
+  },
+
+  // iterates over elements of a collection and invokes iteratee for each element
   forEach: function() {
 
   },
 
-  // STRETCH GOALS!
+  /*******************
+   *  STRETCH GOALS! *
+   *******************/ 
 
+  // creates an array of values by running each element in collection thru the iteratee
   map: function() {
 
   },
 
+  // iterates over elements of a collection and returns all elements that the predicate returns truthy for
   filter: function() {
 
   },
 
-  // SUPER STRETCH GOAL!
+  /*************************
+   *  SUPER STRETCH GOALS! *
+   *************************/ 
 
+  // Reduces the collection to a value which is the accumulated result of running each element
+  // in the collection through an iteratee
   reduce: function() {
     
   }
