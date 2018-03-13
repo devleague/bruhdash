113f5e67f8e1db0cca0d1f6490985d263530897c
cryosx <cyruswu.email@gmail.com>
2018-03-12 20:21:26 -1000

    finished up to Slice()

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,38 +10,46 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (array) {
+      return array[0];
   },
 
   // returns the last element of an array
-  last: function () {
-
+  last: function (array) {
+    return array[array.length - 1];
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
+  indexOf: function (array, value, fromIndex) {
+    return array.indexOf(value, fromIndex);
 
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function (array, value) {
+    return array.lastIndexOf(value);
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (array) {
+    array.pop();
+    return array;
   },
   
   // returns an array with all falsey values removed
-  compact: function() {
-
+  compact: function(array) {
+    let compactArray = [];
+    array.forEach(function(elem){
+      if (elem !== false && elem !== null && elem !== 0 && elem !== "" && elem !== undefined && !isNaN(elem)) {
+        compactArray.push(elem);
+      }
+    });
+    return compactArray;
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (array, start, end) {
+    return array.slice(start, end);
   },
 
   // returns a slice of array with n elements dropped from the beignning
